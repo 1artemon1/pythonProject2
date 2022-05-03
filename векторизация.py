98178c6bd04a10d249bb996fe523c26ebb257bee
EnderPortman <103992395+EnderPortman@users.noreply.github.com>
2022-05-03 08:34:10 +0300

    Add files via upload

---
+++ векторизация.py
@@ -0,0 +1,64 @@
+import collections
+import csv
+from csv import writer
+file = open("filterAll.txt", encoding= "utf-8")
+reviews = file.read().split(sep = '!!!')
+
+print(len(reviews))
+
+def compute_tf(text):
+    tf_text = collections.Counter(text)
+    for i in tf_text:
+        tf_text[i] = tf_text[i]/float(len(text))
+    return tf_text
+
+
+#IDF
+dict = {}
+def analysis(my_list, dict):
+  for i in my_list:
+    if i in dict:
+      dict[i] += 1
+    else:
+      dict[i] = 1
+
+
+for review in reviews:
+    review = review.split()
+    analysis(review, dict)
+
+
+import math
+
+for key in dict:
+  dict[key] = math.log10(25000/dict[key]) #idf dictionary
+print(len(dict), "!!!@!!!")
+
+
+#vectoriztion
+vectors = open("vectors.csv", 'w')
+writer = csv.writer(vectors)
+
+f = 0
+for review in reviews:
+  review = review.split()
+  rev = set(review)
+  s = [0] * 33812
+  i = 0
+  count = compute_tf(review)
+  for key in dict:
+    if key in rev:
+      tfidf = count[key] * dict[key]
+      tfidf = '{:.3f}'.format(tfidf)
+      s[i] =float(tfidf)
+    i += 1
+
+  writer.writerow(s)
+  f += 1
+  print(f)
+
+file.close()
+vectors.close()
+
+#написать размер словаря - размерность пространства заисывать поз и нег в два стлб
+#выкинуть лишние слова, которые нихуя не весят? ну или нет чтоб точность была выше типа

b49e090637375b3a158228203e7afb8a26f94c16
EnderPortman <103992395+EnderPortman@users.noreply.github.com>
2022-05-18 19:51:13 +0300

    Handling

--- обработка.py
+++ обработка.py
@@ -17,150 +17,28 @@ stop_words.add("br")  # перенос стр
 appendFile1 = open('filterPos.txt', 'w')
 appendFile2 = open('filterNeg.txt', 'w')
 
-
 f = 0
-for file in os.scandir(r"C:\Users\sever\OneDrive\Рабочий стол\имдб\aclImdb_v1\aclImdb\train\neg"):
-    fil = open(file.path, encoding='utf-8')
-    words = fil.read().lower().split()  # массив слов в нижнем регистре
+def handling(path, fileW, f):
+    for file in os.scandir(path):
+        fil = open(file.path, encoding='utf-8')
+        words = fil.read().lower().split()  # массив слов в нижнем регистре
+        f += 1
+        print(f)
+        for r in words:
+            if r not in stop_words:  # сначала убираем стоп-слова, тк в некоторых есть '
+                r = re.sub("[^A-Za-z]", "", r)  # удаляем числа и знаки препинания из слова
 
-    f += 1
-    print(f)
-
-    for r in words:
-
-        if r not in stop_words:  # сначала убираем стоп-слова, тк в некоторых есть '
-
-            r = re.sub("[^A-Za-z]", "", r)  # удаляем числа и знаки препинания из слова
             if r != '' and r not in stop_words and r in spell:  # для удаленных чисел ставших пустым местом или слипшихся
-                appendFile2.write(" " + sn_stemmer.stem(r))  # оставляем основу слова
+                fileW.write(" " + sn_stemmer.stem(r))  # оставляем основу слова
+        if f % 25000!=0:
+            fileW.write("!!!")  # разделитель отзывов
+        fil.close()
+    return f
 
-    appendFile2.write("!!!")  # разделитель отзывов
-    fil.close()
-
-for file in os.scandir(r"C:\Users\sever\OneDrive\Рабочий стол\имдб\aclImdb_v1\aclImdb\test\neg"):
-    fil = open(file.path, encoding='utf-8')
-    words = fil.read().lower().split()  # массив слов в нижнем регистре
-
-    f += 1
-    print(f)
-
-    for r in words:
-
-        if r not in stop_words:  # сначала убираем стоп-слова, тк в некоторых есть '
-
-            r = re.sub("[^A-Za-z]", "", r)  # удаляем числа и знаки препинания из слова
-            if r != '' and r not in stop_words and r in spell:  # для удаленных чисел ставших пустым местом или слипшихся
-                appendFile2.write(" " + sn_stemmer.stem(r))  # оставляем основу слова
-    if f!=25000:
-        appendFile2.write("!!!")  # разделитель отзывов
-    fil.close()
-
+f = handling(r"C:\Users\sever\OneDrive\Рабочий стол\имдб\aclImdb_v1\aclImdb\train\neg", appendFile2, f)
+f = handling(r"C:\Users\sever\OneDrive\Рабочий стол\имдб\aclImdb_v1\aclImdb\test\neg", appendFile2, f)
 appendFile2.close()
 
-for file in os.scandir(r"C:\Users\sever\OneDrive\Рабочий стол\имдб\aclImdb_v1\aclImdb\train\pos"):
-    fil = open(file.path, encoding='utf-8')
-    words = fil.read().lower().split()  # массив слов в нижнем регистре
-
-    f += 1
-    print(f)
-
-    for r in words:
-
-        if r not in stop_words:  # сначала убираем стоп-слова, тк в некоторых есть '
-
-            r = re.sub("[^A-Za-z]", "", r)  # удаляем числа и знаки препинания из слова
-            if r != '' and r not in stop_words and r in spell:  # для удаленных чисел ставших пустым местом или слипшихся
-                appendFile1.write(" " + sn_stemmer.stem(r))  # оставляем основу слова
-
-    appendFile1.write("!!!")  # разделитель отзывов
-    fil.close()
-
-for file in os.scandir(r"C:\Users\sever\OneDrive\Рабочий стол\имдб\aclImdb_v1\aclImdb\test\pos"):
-    fil = open(file.path, encoding='utf-8')
-    words = fil.read().lower().split()  # массив слов в нижнем регистре
-
-    f += 1
-    print(f)
-
-    for r in words:
-
-        if r not in stop_words:  # сначала убираем стоп-слова, тк в некоторых есть '
-
-            r = re.sub("[^A-Za-z]", "", r)  # удаляем числа и знаки препинания из слова
-            if r != '' and r not in stop_words and r in spell:  # для удаленных чисел ставших пустым местом или слипшихся
-                appendFile1.write(" " + sn_stemmer.stem(r))  # оставляем основу слова
-    if f!= 50000:
-        appendFile1.write("!!!")  # разделитель отзывов
-    fil.close()
-
+f = handling(r"C:\Users\sever\OneDrive\Рабочий стол\имдб\aclImdb_v1\aclImdb\train\pos", appendFile1, f)
+handling(r"C:\Users\sever\OneDrive\Рабочий стол\имдб\aclImdb_v1\aclImdb\test\pos", appendFile1, f)
 appendFile1.close()
-for file in os.scandir(r"C:\Users\asmis\OneDrive\Рабочий стол\train\pos"):
-    fil = open(file.path, encoding='utf-8')
-    words = fil.read().lower().split()  # массив слов в нижнем регистре
-
-    f += 1
-    print(f)
-
-    for r in words:
-
-        if r not in stop_words:  # сначала убираем стоп-слова, тк в некоторых есть '
-
-            r = re.sub("[^A-Za-z]", "", r)  # удаляем числа и знаки препинания из слова
-            if r != '' and r not in stop_words and r in spell:  # для удаленных чисел ставших пустым местом или слипшихся
-                appendFile.write(" " + sn_stemmer.stem(r))  # оставляем основу слова
-
-    appendFile.write("!!!")  # разделитель отзывов
-    fil.close()
-
-appendFile2 = open('filterTest.txt', 'w')
-
-for file in os.scandir(r"C:\Users\asmis\OneDrive\Рабочий стол\test\neg"):
-    fil = open(file.path, encoding='utf-8')
-    words = fil.read().lower().split()  # массив слов в нижнем регистре
-
-    f += 1
-    print(f)
-
-    for r in words:
-
-        if r not in stop_words:  # сначала убираем стоп-слова, тк в некоторых есть '
-
-            r = re.sub("[^A-Za-z]", "", r)  # удаляем числа и знаки препинания из слова
-            if r != '' and r not in stop_words and r in spell:  # для удаленных чисел ставших пустым местом или слипшихся
-                if f > 25000 and f <= 30000:  # еще 5к отзывов из теста в трэйн добавляем
-                    appendFile.write(" " + sn_stemmer.stem(r))  # оставляем основу слова
-                else:
-                    appendFile2.write(" " + sn_stemmer.stem(r))  # оставляем основу слова
-    if f > 25000 and f <= 30000:
-        appendFile.write("!!!")  # разделитель отзывов
-    else:
-        appendFile2.write("!!!")  # разделитель отзывов
-    fil.close()
-
-for file in os.scandir(r"C:\Users\asmis\OneDrive\Рабочий стол\test\pos"):
-    fil = open(file.path, encoding='utf-8')
-    words = fil.read().lower().split()  # массив слов в нижнем регистре
-
-    f += 1
-    print(f)
-
-    for r in words:
-
-        if r not in stop_words:  # сначала убираем стоп-слова, тк в некоторых есть '
-
-            r = re.sub("[^A-Za-z]", "", r)  # удаляем числа и знаки препинания из слова
-            if r != '' and r not in stop_words and r in spell:  # для удаленных чисел ставших пустым местом или слипшихся
-                if f > 45000 and f <= 50000:  # еще 5к отзывов из теста в трэйн добавляем
-                    appendFile.write(" " + sn_stemmer.stem(r))  # оставляем основу слова
-                else:
-                    appendFile2.write(" " + sn_stemmer.stem(r))  # оставляем основу слова
-    if f > 45000 and f < 50000:
-        appendFile.write("!!!")  # разделитель отзывов
-    elif f != 50000:
-        appendFile2.write("!!!")  # разделитель отзывов
-
-    fil.close()
-
-appendFile.close()
-
-appendFile2.close()
